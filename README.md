af9647dcdc68f76449fb37cd5fa5c14ce32777f4
Nahid <nahidhassan25@gmail.com>
2017-10-08 20:57:34 -0400

    Added script to automate the maps naming

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-kf2-adding-maps-to-server
+# **kf2-adding-maps-to-server**
+
+To use:
+Drop this script to the folder where you have all of the maps (They must end in .kfm extenshion)
+Double click to execute and it will create a file called maps.txt
+Then all you have to do is copy and paste the information to your PCServer-KFGame.ini folder of your server.
+
+Note this is does not act as a server for people to get the files from, but you do need this in your maps folder.
+A good server I would recommend is http://skillzservers.com/kf2-redirect/
+To use skillz simple add 
+```
+[IpDrv.HTTPDownload]
+RedirectToURL=
+RedirectToURL=http://skillzservers.com/kf2-redirect/
+```
+to your PCServer-KFEngine.ini file
---
+++ kf2_create_maps.py
@@ -0,0 +1,23 @@
+#Author: Nahid Sarker
+#Date created: 10/8/2017
+#Date modified: 10/8/2017
+#Purpose: Looks in current directory for all kf2 map files and writes it in the format of:
+'''
+[MAPNAME.kfm KFMapSummary]
+MapName=MAPNAME
+MapAssociation=2
+ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder
+'''
+#Created on: Windows 10 using Pycharm
+#Tested on: Windows 10
+
+import os
+
+f = open("maps.txt", "w+")
+arr_txt = [x for x in os.listdir('.') if x.endswith(".kfm")]
+for map in arr_txt:
+    f.write("[%s KFMapSummary]\n" % (map))
+    f.write("MapName=%s\n" % (map.rsplit('.', 1)[0]))
+    f.write('MapAssociation=2\n')
+    f.write('ScreenshotPathName=UI_MapPreview_TEX.UI_MapPreview_Placeholder\n')
+    f.write('\n')
